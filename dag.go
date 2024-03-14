d4d824366d5452a59c6f9ab269f905fa4b4a5969
ranqiao2002 <132678823+ranqiao2002@users.noreply.github.com>
2024-03-14 21:44:08 +0800

    Update dag.go

--- dag.go
+++ dag.go
@@ -15,5 +15,34 @@ type Object struct {
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
 	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+	switch node.Type() {
+	case FILE:
+		file := node.(File) // 将node转换为File类型
+		// 将文件内容写入到KVStore中
+		err := store.Put([]byte("file"), file.Bytes())
+		if err != nil {
+			panic(err)
+		}
+	case DIR:
+		dir := node.(Dir) // 将node转换为Dir类型
+		it := dir.It()    // 获取DirIterator
+		for it.Next() {
+			childNode := it.Node()
+			// 递归调用Add函数处理子节点
+			Add(store, childNode, h)
+		}
+	}
+	
+	
+	// 计算Merkle Root
+	root := h.Sum(nil)
+
+	// 将Merkle Root写入到KVStore中
+	err := store.Put([]byte("root"), root)
+	if err != nil {
+		panic(err)
+	}
+
+	return root
 }
+
